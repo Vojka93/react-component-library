444da8fe57e34ca398f08fdf2d043dd803f13a23
Vojin Doncev <vojka1993@gmail.com>
2024-05-02 10:39:47 +0200

    edit rollup config file

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -9,6 +9,7 @@ import packageJson from './package.json' assert { type: 'json' }
 export default [
   {
     input: 'src/index.ts',
+    external: ['react', 'react-dom'],
     output: [
       {
         file: packageJson.main,
